480d0dac31849985adf9e327d571f641c9d4a31a
mitchgoertzen <mitchgoertzen@gmail.com>
2026-07-06 13:04:55 -0700

    update prerender api

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,36 +1,27 @@
 import { contextBridge, ipcRenderer } from 'electron';
 import { electronAPI } from '@electron-toolkit/preload';
 
-// Custom APIs for renderer
-const api = {};
+const api = {
+  onReceiveData: (callback) => {
+    ipcRenderer.removeAllListeners('recv-album');
+    ipcRenderer.once('recv-album', (_event, value) => callback(value));
+  },
+  onFolderSelected: (callback) => {
+    ipcRenderer.removeAllListeners('folder-select');
+    ipcRenderer.once('folder-select', (_event, value) => callback(value));
+  },
+  onScanComplete: (callback) => {
+    ipcRenderer.removeAllListeners('scan-complete');
+    ipcRenderer.once('scan-complete', (_event, value) => callback(value));
+  },
+  onSaveComplete: (callback) => {
+    ipcRenderer.removeAllListeners('save-complete');
+    ipcRenderer.on('save-complete', (_event, value) => callback(value));
+  }
+};
 
-// Use `contextBridge` APIs to expose Electron APIs to
-// renderer only if context isolation is enabled, otherwise
-// just add to the DOM global.
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld('test', {
-      onReceiveData: (callback) => {
-        //   console.log('preload data');
-        ipcRenderer.removeAllListeners('recv-album');
-        ipcRenderer.once('recv-album', (_event, value) => callback(value));
-      },
-      onFolderSelected: (callback) => {
-        //   console.log('preload folder');
-        ipcRenderer.removeAllListeners('folder-select');
-        ipcRenderer.once('folder-select', (_event, value) => callback(value));
-      },
-      onScanComplete: (callback) => {
-        // console.log('preload scan');
-        ipcRenderer.removeAllListeners('scan-complete');
-        ipcRenderer.once('scan-complete', (_event, value) => callback(value));
-      },
-      onSaveComplete: (callback) => {
-        // console.log('preload scan');
-        ipcRenderer.removeAllListeners('save-complete');
-        ipcRenderer.on('save-complete', (_event, value) => callback(value));
-      }
-    });
     contextBridge.exposeInMainWorld('electron', electronAPI);
     contextBridge.exposeInMainWorld('api', api);
   } catch (error) {
